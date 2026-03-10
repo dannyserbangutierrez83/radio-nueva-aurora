241063ccf70c40f0193d55bc36efe30bb2382352
UTEC Danny <danny.gutierrez@estudiantes.utec.edu.uy>
2026-03-10 18:46:52 -0300

    build: renombra APK a radioSNA-1.0.apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,12 @@ android {
         }
     }
 
+    applicationVariants.all {
+        outputs.all {
+            (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName = "radioSNA-${versionName}.apk"
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
